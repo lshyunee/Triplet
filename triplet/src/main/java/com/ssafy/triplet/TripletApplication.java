0cee64913bed4340f22d1dbd9dfc87e78951de03
jinseobYun <wlstjq447@naver.com>
2024-09-10 23:43:53 +0900

    Init: SpringBoot 초기설정

---
+++ triplet/src/main/java/com/ssafy/triplet/TripletApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.triplet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripletApplication.class, args);
+	}
+
+}
