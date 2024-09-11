17c4e2218d922278dc5d4f136fcb2b4627c9889b
hyun <a94460093@gmail.com>
2024-09-11 13:41:00 +0900

    Init: WebConfig

---
+++ triplet/src/main/java/com/ssafy/triplet/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.ssafy.triplet.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOriginPatterns("*")// 허용할 출처 : 특정 도메인만 받을 수 있음
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(1800); // Pre-flight Caching
+    }
+
+}
