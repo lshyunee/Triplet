61fd619fa24433bb8940d2ff8c809898e92d7efc
enghddd <jung1213546@gmail.com>
2024-09-17 12:57:31 +0900

    Feat: 휴대폰번호 유효성검사 추가

--- triplet/src/main/java/com/ssafy/triplet/sms/controller/SmsController.java
+++ triplet/src/main/java/com/ssafy/triplet/sms/controller/SmsController.java
@@ -1,13 +1,16 @@
 package com.ssafy.triplet.sms.controller;
 
 import com.ssafy.triplet.response.ApiResponse;
+import com.ssafy.triplet.response.CustomErrorCode;
 import com.ssafy.triplet.sms.dto.request.SmsConfirmRequest;
 import com.ssafy.triplet.sms.dto.request.SmsSendRequest;
 import com.ssafy.triplet.sms.service.SmsService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,13 +25,19 @@ public class SmsController {
     private final SmsService smsService;
 
     @PostMapping("/send")
-    public ResponseEntity<?> sendSMS(@RequestBody SmsSendRequest request){
+    public ResponseEntity<?> sendSMS(@Valid @RequestBody SmsSendRequest request, BindingResult bindingResult){
+        if (bindingResult.hasErrors()) {
+            return new ResponseEntity<>(ApiResponse.isError(CustomErrorCode.INVALID_PHONE_NUMBER), HttpStatus.BAD_REQUEST);
+        }
         smsService.sendSms(request);
         return new ResponseEntity<>(new ApiResponse<Void>("200", "인증번호 요청 성공"), HttpStatus.OK);
     }
 
     @PostMapping("/confirm")
-    public ResponseEntity<?> confirmSMS(@RequestBody SmsConfirmRequest request){
+    public ResponseEntity<?> confirmSMS(@Valid @RequestBody SmsConfirmRequest request, BindingResult bindingResult){
+        if (bindingResult.hasErrors()) {
+            return new ResponseEntity<>(ApiResponse.isError(CustomErrorCode.INVALID_PHONE_NUMBER), HttpStatus.BAD_REQUEST);
+        }
         if (smsService.verifySms(request)) {
             log.info("인증 성공");
             return new ResponseEntity<>(new ApiResponse<Void>("200", "인증 확인"), HttpStatus.OK);
--- triplet/src/main/java/com/ssafy/triplet/sms/dto/request/SmsConfirmRequest.java
+++ triplet/src/main/java/com/ssafy/triplet/sms/dto/request/SmsConfirmRequest.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 @Getter
 public class SmsConfirmRequest {
 
-    @NotNull(message = "전화번호를 입력하세요.")
+    @NotNull(message = "전화번호는 10~11자리의 숫자여야 합니다.")
     @Pattern(regexp = "^[0-9]{10,11}$", message = "전화번호는 10~11자리의 숫자여야 합니다.")
     private String phoneNumber;
 
