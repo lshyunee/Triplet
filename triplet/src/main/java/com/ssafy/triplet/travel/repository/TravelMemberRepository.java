0424333c4995fab7f7bfd90c694144b9da6d89a4
hyun <a94460093@gmail.com>
2024-09-19 17:16:37 +0900

    feat: 여행 공유 기능 추가 - service interface 삭제 - keyword 삭제 Closes #11

--- triplet/src/main/java/com/ssafy/triplet/travel/repository/TravelMemberRepository.java
+++ triplet/src/main/java/com/ssafy/triplet/travel/repository/TravelMemberRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TravelMemberRepository  extends JpaRepository<TravelMember, Long> {
+public interface TravelMemberRepository extends JpaRepository<TravelMember, Long> {
 
 }
