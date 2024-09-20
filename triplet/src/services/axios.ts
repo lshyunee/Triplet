26c652ea8e66cf7d3c647d3a7bc481df940d2664
김문희 <homoonshi@gmail.com>
2024-09-20 14:51:02 +0900

    Fix : 로그인 axios 주소 변경

--- triplet/src/services/axios.ts
+++ triplet/src/services/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL : process.env.REACT_APP_API_URL,
+    baseURL : 'http://localhost:8080',
     timeout : 10000,
     withCredentials : true,
 });
