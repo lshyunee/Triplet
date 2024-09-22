76c07c3d6ee8d6a47f7cafec7b1181a2ef53ee4f
homoonshi <homoonshi@gmail.com>
2024-09-22 23:06:49 +0900

    Feat : 여행 컴포넌트 CSS 추가

--- triplet/src/pages/home/HomePage.tsx
+++ triplet/src/pages/home/HomePage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../../components/header/Header';
+import TravelCard from '../../components/travel/TravelCard';
 
 const HomePage = () => {
 
@@ -7,6 +8,7 @@ const HomePage = () => {
     return (
         <>
             <Header/>
+            <TravelCard/>
             <div>홈</div>
         </>
     );
