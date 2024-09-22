4d218fa5ac064cb6ae55f35340f4987e1d861cd1
homoonshi <homoonshi@gmail.com>
2024-09-22 21:33:49 +0900

    Fix : 로그인페이지 useAxios 훅 사용 수정

--- triplet/src/pages/user/login/LoginPage.tsx
+++ triplet/src/pages/user/login/LoginPage.tsx
@@ -4,7 +4,7 @@ import { useNavigate, Link } from 'react-router-dom';
 import { loginSuccess } from '../../../features/auth/authSlice';
 import styled from 'styled-components';
 
-import { login, naverLogin } from '../../../services/user/authService';
+import useAxios from '../../../hooks/useAxios';
 
 const BigDiv = styled.div`
     display:flex;
@@ -74,35 +74,6 @@ const LoginPage = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
-    const handleLogin = () => {
-        const user = {
-            id : id.value,
-            pw : pw.value,
-        }
-        
-        const Login = async () => {
-            const response = await login(user.id, user.pw);
-            if(response===200){
-                dispatch(loginSuccess());
-                navigate('/home');
-            }
-        }
-
-        Login();
-    }
-
-    const handleNaverLogin = () => {
-        const Login = async () => {
-            const response = await naverLogin();
-            if(response===200){
-                dispatch(loginSuccess());
-                navigate('/home');
-            }
-        }
-
-        Login();
-    }
-
     const useInput = (validator:Function) => {
 
         const [ value, setValue ] = useState('');
@@ -125,6 +96,7 @@ const LoginPage = () => {
         return { value, onChange };
     }
 
+    
     const validId = (value:string): boolean => {
         const regex = /^[a-zA-z0-9]*$/;
         return value.length <= 16 && regex.test(value);
@@ -137,6 +109,40 @@ const LoginPage = () => {
 
     const id = useInput(validId);
     const pw = useInput(validPw);
+
+    const formData = new FormData();
+    formData.append('memberId', id.value);
+    formData.append('password', pw.value);
+
+    const { data: loginData, error: loginError, loading: loginLoading
+        , status: loginStatus, refetch: loginRefetch } 
+        = useAxios('/login', 'POST', formData);
+
+        const { data: naverData, error: naverError, loading: naverLoading,
+            status: naverStatus, refetch: naverRefetch }
+            = useAxios('/oauth2/authorization/naver', 'POST');
+
+
+    const handleLogin = () => {
+
+        if( loginStatus === 200){
+            dispatch(loginSuccess());
+            navigate('/home');
+        }else if( loginStatus === 400 ){
+            console.log("로그인 정보가 잘못되었습니다.");
+        }
+    }
+
+    const handleNaverLogin = () => {
+    
+        if( naverStatus === 200){
+            dispatch(loginSuccess());
+            navigate('/home');
+        }else if( naverStatus === 400 ){
+            console.log("로그인 정보가 잘못되었습니다.");
+        }
+
+    }
 
     return (
         <>
