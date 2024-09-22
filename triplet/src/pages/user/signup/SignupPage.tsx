e6cd308820496f2a88556e55d9ef97431431d21e
homoonshi <homoonshi@gmail.com>
2024-09-22 21:34:26 +0900

    Feat : 회원가입 중복확인, 인증번호 발송, 확인 기능 추가, css 변경

--- triplet/src/pages/user/signup/SignupPage.tsx
+++ triplet/src/pages/user/signup/SignupPage.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import BackHeader from '../../../components/header/BackHeader';
 import styled from 'styled-components';
+import { useDispatch } from 'react-redux';
+import useAxios from '../../../hooks/useAxios';
 
 const HowP = styled.p`
     font-size : 12px;
     margin-bottom : 3px;
     color : #888888;
+`;
+
+const ExplainP = styled.p`
+    font-size : 10px;
+    padding-top : 2px;
+    margin-bottom : 3px;
+    margin-left : 4px;
+    color : #AAAAAA;
 `;
 
 const NumP = styled.p`
@@ -15,11 +25,22 @@ const NumP = styled.p`
     margin-right : 12px;
 `;
 
+const CheckP = styled.p`
+    font-size : 12px;
+    color : #008DE7;
+    margin-top: 0;
+`
+
 const InputDiv = styled.div`
     display: flex;
     flex-direction: column;
     padding: 20px;
     margin-top:20px;
+`;
+
+const ExplainDiv = styled.div`
+    display : flex;
+    flex-direction: row;
 `;
 
 const StyledInput = styled.input`
@@ -51,6 +72,7 @@ const CheckDiv = styled.div`
 
     ${StyledInput} {
         width:246px;
+        margin-bottom: 4px;
     }
 `;
 
@@ -58,6 +80,7 @@ const StyledBtn = styled.button`
     width:74px;
     height:44px;
     border-radius : 10px;
+    font-weight: 600;
     background-color : white;
     color : #008DE7;
     border : 1px solid #008DE7;
@@ -107,6 +130,8 @@ const ConfirmBox = styled.button`
     background-color : #008DE7;
     color : #FFFFFF;
     border-radius : 10px;
+    font-weight: 600;
+    font-size : 14px;
     border : none;
     margin-top : 60px;
 `;
@@ -115,42 +140,147 @@ const SignupPage = () => {
 
     const title = '회원가입';
 
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const useInput = (validator?: (value: string) => boolean) => {
+        const [value, setValue] = useState('');
+      
+        const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+          const {
+            target: { value },
+          } = event;
+      
+          // validator가 없으면 기본적으로 true로 설정
+          let willUpdate = true;
+      
+          // validator가 제공된 경우만 실행
+          if (validator && typeof validator === 'function') {
+            willUpdate = validator(value);
+          }
+      
+          // validator 통과한 경우에만 value 업데이트
+          if (willUpdate) {
+            setValue(value);
+          }
+        };
+      
+        return { value, onChange };
+      };
+      
+
+    const validId = (value:string) : boolean => {
+        const regex = /^[a-zA-z0-9]*$/;
+        return value.length <= 16 && regex.test(value);
+    }
+
+    const validPw = (value:string): boolean => {
+        const regex = /^[a-zA-Z0-9!@#$%^&*()]*$/;
+        return value.length <= 15 && regex.test(value);
+    }
+
+    const id = useInput(validId);
+    const pw = useInput(validPw);
+    const pwCheck = useInput(validPw);
+    const name = useInput();
+    const identificationNumFront = useInput();
+    const identificationNumBack = useInput();
+    const phoneNumFront = useInput();
+    const phoneNumMiddle = useInput();
+    const phoneNumBack = useInput();
+    const certificationNum = useInput();
+
+    const [ phoneNum, setPhoneNum ] = useState('');
+
+    // 아이디 중복 체크
+    const { data: duplicatedData, error: duplicatedError, loading: duplicatedLoading,
+        status: duplicatedStatus, refetch: duplicatedRefetch }
+        = useAxios('/signup/is-duplicated', 'POST', {memberId : id.value});
+
+    const [ isDuplicated, setIsDuplicated ] = useState(true);
+
+    const idDuplicationCheck = () => {
+        // 중복 체크 요청을 실행하기 위해 refetch 호출
+        duplicatedRefetch();
+    };
+
+    useEffect(() => {
+        // 상태 업데이트를 불필요하게 반복하지 않도록, 명확한 조건 설정
+        if (duplicatedStatus === 200 || duplicatedStatus === 401) {
+          // 상태를 이미 업데이트한 경우 중복 호출 방지
+          if (isDuplicated !== duplicatedData.isDuplicated) {
+            setIsDuplicated(duplicatedData.isDuplicated);
+          }
+        }
+        // 의존성 배열에서 duplicatedData를 제외하여 불필요한 상태 변경 방지
+      }, [duplicatedStatus]); // duplicatedStatus만 의존성 배열에 포함
+
+      // 전화번호 인증
+    useEffect(() => {
+        setPhoneNum(`${phoneNumFront}${phoneNumMiddle}${phoneNumBack}`);
+    }, [phoneNumFront, phoneNumMiddle, phoneNumBack])
+
+    const { data: phoneData, error: phoneError, loading: phoneLoading,
+        status: phoneStatus, refetch: phoneRefetch }
+        = useAxios('/sms/send','POST',{phoneNumber : phoneNum});
+
+    const certificateSend = () => {
+        phoneRefetch();
+    };
+
+    const { data : smsData, error: smsError, loading: smsLoading,
+        status: smsStatus, refetch: smsRefetch}
+        = useAxios('/sms/confirm', 'POST', 
+            {phoneNumber : phoneNum, certificationNumber: certificationNum});
+
+    const certificateCheck = () => {
+        smsRefetch();
+    }
+
     return(
         <div>
             <BackHeader title={title}/>
             <InputDiv>
-                <HowP>아이디</HowP>
+                <ExplainDiv>
+                    <HowP>아이디</HowP>
+                    <ExplainP>(영문, 숫자 포함 5~16자)</ExplainP>
+                </ExplainDiv>
                 <CheckDiv>
-                    <StyledInput type="text" />
-                    <StyledBtn>중복확인</StyledBtn>
+                    <StyledInput type="text" {...id} />
+                    <StyledBtn onClick={idDuplicationCheck}>중복확인</StyledBtn>
                 </CheckDiv>
-                <HowP>비밀번호</HowP>
-                <StyledInput type="text" />
+                <CheckP>{isDuplicated ? " " : "사용 가능한 아이디입니다."}</CheckP>
+                <ExplainDiv>
+                    <HowP>비밀번호</HowP>
+                    <ExplainP>(영문, 숫자, 특수문자 포함 8~15자)</ExplainP>
+                </ExplainDiv>
+                <StyledInput type="password" {...pw}/>
                 <HowP>비밀번호 확인</HowP>
-                <StyledInput type="text" />
+                <StyledInput type="password" {...pwCheck} />
                 <HowP>이름</HowP>
-                <StyledInput type="text" />
+                <StyledInput type="text" {...name}/>
                 <HowP>주민등록번호</HowP>
                 <RegistDiv>
-                    <StyledInputFront type="text" />
+                    <StyledInputFront type="text" {...identificationNumFront}/>
                     <NumP>-</NumP>
-                    <StyledInput type="text" />
+                    <StyledInput type="text" {...identificationNumBack}/>
                     <NumP>* * * * * *</NumP>
                 </RegistDiv>
                 <HowP>전화번호</HowP>
                 <PhoneDiv>
-                    <StyledInputFront type="text" />
+                    <StyledInputFront type="text" {...phoneNumFront}/>
                     <NumP>-</NumP>
-                    <StyledInput type="text" />
+                    <StyledInput type="text" {...phoneNumMiddle}/>
                     <NumP>-</NumP>
-                    <StyledInput type="text" />
-                    <StyledBtn>인증번호 발송</StyledBtn>
+                    <StyledInput type="text" {...phoneNumBack}/>
+                    <StyledBtn onClick={certificateSend}>인증번호 발송</StyledBtn>
                 </PhoneDiv>
                 <HowP>인증번호</HowP>
                 <CheckDiv>
-                    <StyledInput type="text" />
-                    <StyledBtn>확인</StyledBtn>
+                    <StyledInput type="text" {...certificationNum} />
+                    <StyledBtn onClick={certificateCheck}>확인</StyledBtn>
                 </CheckDiv>
+                <CheckP>인증되었습니다.</CheckP>
                 <ConfirmBox>회원가입</ConfirmBox>
             </InputDiv>
         </div>
