1d26e7e636a039662395020f910a2c9d0cb51e5b
hyun <a94460093@gmail.com>
2024-09-23 15:51:14 +0900

    feat: 여행 카테고리별 지출 비용 현황 조회 기능 추가 - memberRepository findIdByMemberId 수정 - 여행 예산 설정, 수정에서 예산의 50%, 80% 저장 로직 추가 Closes #29

--- triplet/src/main/java/com/ssafy/triplet/member/repository/MemberRepository.java
+++ triplet/src/main/java/com/ssafy/triplet/member/repository/MemberRepository.java
@@ -2,8 +2,12 @@ package com.ssafy.triplet.member.repository;
 
 import com.ssafy.triplet.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Member findByMemberId(String memberId);
-    Long findIdByMemberId(String memberId);
+
+    @Query("SELECT m.id FROM Member m WHERE m.memberId = :memberId")
+    Long findIdByMemberId(@Param("memberId") String memberId);
 }
