c2a476ed96d930ed562fa98ab86f5a12ea3bc983
hyun <a94460093@gmail.com>
2024-09-24 13:50:27 +0900

    refactor: 여행 생성 시 여행 계좌 자동 생성 Closes #34

---
+++ triplet/src/main/java/com/ssafy/triplet/travel/repository/TravelWalletRepository.java
@@ -0,0 +1,7 @@
+package com.ssafy.triplet.travel.repository;
+
+import com.ssafy.triplet.travel.entity.TravelWallet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TravelWalletRepository extends JpaRepository<TravelWallet, Long> {
+}
