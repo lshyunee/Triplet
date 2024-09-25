9c54b71504f4abed50e8048f9638cfc1512e72e8
enghddd <jung1213546@gmail.com>
2024-09-25 11:13:32 +0900

    Refactor: 토큰 검증 에러메시지 추가

--- triplet/src/main/java/com/ssafy/triplet/auth/jwt/JwtFilter.java
+++ triplet/src/main/java/com/ssafy/triplet/auth/jwt/JwtFilter.java
@@ -1,5 +1,6 @@
 package com.ssafy.triplet.auth.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssafy.triplet.auth.dto.CustomUserPrincipal;
 import com.ssafy.triplet.auth.dto.MemberAuthDto;
 import io.jsonwebtoken.ExpiredJwtException;
@@ -10,6 +11,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -17,7 +19,9 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -29,6 +33,7 @@ public class JwtFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         // 아래 경로에 포함되면 jwt검사 생략
         List<String> whiteList = List.of(
+                "/error",
                 "/api/v1/login",
                 "/api/v1/signup",
                 "/api/v1/reissue",
@@ -56,7 +61,19 @@ public class JwtFilter extends OncePerRequestFilter {
         // access 토큰 검증
         if (accessToken == null) {
             log.info("token null");
-            filterChain.doFilter(request, response);
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+
+            Map<String, Object> responseBody = new HashMap<>();
+            responseBody.put("code", "M0011");
+            responseBody.put("message", "토큰이 비어있습니다.");
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            String jsonResponse = objectMapper.writeValueAsString(responseBody);
+
+            // JSON 응답 쓰기
+            response.getWriter().write(jsonResponse);
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
             return; // 조건에 해당되면 메서드 종료
         }
 
@@ -65,10 +82,19 @@ public class JwtFilter extends OncePerRequestFilter {
             jwtUtil.isExpired(accessToken);
         } catch (ExpiredJwtException e) { // 만료된 토큰이면 다음 필터로 넘기지 않음
             log.info("token expired");
-            PrintWriter writer = response.getWriter();
-            writer.print("access token expired");
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
 
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            Map<String, Object> responseBody = new HashMap<>();
+            responseBody.put("code", "M0012");
+            responseBody.put("message", "토큰이 만료되었습니다.");
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            String jsonResponse = objectMapper.writeValueAsString(responseBody);
+
+            // JSON 응답 쓰기
+            response.getWriter().write(jsonResponse);
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
             return;
         }
 
@@ -77,10 +103,19 @@ public class JwtFilter extends OncePerRequestFilter {
         // 토큰이 access 인지 확인
         if (!"access".equals(category)) {
             log.info("invalid access token");
-            PrintWriter writer = response.getWriter();
-            writer.print("invalid access token");
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
 
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            Map<String, Object> responseBody = new HashMap<>();
+            responseBody.put("code", "M0018");
+            responseBody.put("message", "토큰이 유효하지 않습니다.");
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            String jsonResponse = objectMapper.writeValueAsString(responseBody);
+
+            // JSON 응답 쓰기
+            response.getWriter().write(jsonResponse);
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
             return;
         }
 
