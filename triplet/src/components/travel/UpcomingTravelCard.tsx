bd34f5e8243deeaa6d69f575525e4cee5660900f
homoonshi <homoonshi@gmail.com>
2024-09-28 10:22:28 +0900

    Feat : 여행 페이지 CSS 추가

---
+++ triplet/src/components/travel/UpcomingTravelCard.tsx
@@ -0,0 +1,116 @@
+import React from 'react';
+import styled from 'styled-components';
+import SampleImg from '../../assets/travelSampleImg/sampleImg.png';
+
+const CardDiv = styled.div`
+  width: 156px;
+  height: 156px; /* 카드 높이를 좀 더 늘림 */
+  border-radius: 20px;
+  overflow: hidden;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end; /* 아래쪽에 텍스트 배치 */
+`;
+
+const TravelImg = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 1;
+`;
+
+const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.4); /* 반투명한 검정 오버레이 */
+  z-index: 2;
+`;
+
+const BottomOverlay = styled.div`
+  position: relative;
+  width: 100%;
+  height: 63px;
+  background-color: rgba(0, 0, 0, 0.5); /* 하단 반투명 오버레이 */
+  backdrop-filter: blur(3px); /* 블러 처리 추가 */
+  z-index: 3;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const Badge = styled.div`
+  width: 70px;
+  height: 30px;
+  background-color: rgba(0, 0, 0, 0.5);
+  border-radius: 50px;
+  border: 1px solid white;
+  font-size: 12px;
+  font-weight: 500;
+  color: white;
+  display: flex; /* Flexbox 사용 */
+  justify-content: center; /* 수평 중앙 정렬 */
+  align-items: center; /* 수직 중앙 정렬 */
+  z-index: 4;
+  position: absolute;
+  top: 10px;
+  right: 10px;
+`;
+
+const ContentTitleDiv = styled.div`
+    display : flex;
+    flex-direction : row;
+    justify-content : space-between;
+    margin-bottom : 8px;
+`;
+
+const ContentDiv = styled.div`
+  z-index: 4;
+  color: white;
+  font-size: 14px;
+  font-weight: 600;
+  display: flex;
+    flex-direction : column;
+  padding: 12px;
+`;
+
+const PriceInfoP = styled.p`
+    font-weight: 400;
+    font-size : 12px;
+    color : white;
+    margin: 0;
+`;
+
+const CountryP = styled.p`
+    font-weight : 500;
+    font-size : 14px;
+    color : white;
+    margin: 0;
+`
+
+const UpcomingTravelCard = () => {
+  return (
+    <CardDiv>
+      <TravelImg src={SampleImg} alt="Travel" />
+      <Overlay />
+      <Badge>맛집 투어</Badge>
+      <BottomOverlay>
+        <ContentDiv>
+            <ContentTitleDiv>
+                <CountryP>집에가고싶다</CountryP>
+                <PriceInfoP>대한민국</PriceInfoP>
+            </ContentTitleDiv>
+            <PriceInfoP>24.09.28 ~ 24.10.01</PriceInfoP>
+        </ContentDiv>
+      </BottomOverlay>
+    </CardDiv>
+  );
+};
+
+export default UpcomingTravelCard;
--- triplet/src/pages/travels/TravelsPage.tsx
+++ triplet/src/pages/travels/TravelsPage.tsx
@@ -2,6 +2,63 @@ import React, {useEffect} from 'react';
 import Header from '../../components/header/Header';
 import { useDispatch } from 'react-redux';
 import { pageMove } from '../../features/navigation/naviSlice';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+import TravelCard from '../../components/travel/TravelCard';
+import UpcomingTravelCard from '../../components/travel/UpcomingTravelCard';
+import CompleteTravelCard from '../../components/travel/CompleteTravelCard';
+
+import { ReactComponent as CreateBtn } from '../../assets/travel/create.svg';
+
+const TravelDiv = styled.div`
+    min-height : calc(100vh - 56px);
+    background-color : #F3F4F6;
+    display : flex;
+    flex-direction : column;
+    margin : 56px 0;
+    padding : 0 16px;
+`;
+
+const CategoryP = styled.p`
+    font-size : 14px;
+    font-weight : 500;
+    color : #888888;
+    padding : 0;
+    margin-bottom : 8px;
+`
+
+const OngoingTravelDiv = styled.div`
+    display : flex;
+    flex-direction : column;
+    margin-top : 12px;
+    margin-bottom : 28px;
+`
+
+const UpcomingTravelDiv = styled.div`
+    display : flex;
+    flex-direction : column;
+    margin-bottom : 12px;
+`
+
+const CompleteTravelDiv = styled.div`
+    display : flex;
+    flex-direction : column;
+    margin-bottom : 32px;
+`
+
+const TravelCardDiv = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(156px, 1fr));
+    gap: 16px; /* 카드 사이에 16px의 간격 추가 */
+    justify-content: center;
+`;
+
+const CreateTravelDiv = styled.div`
+    display : flex;
+    justify-content : right;
+    margin-bottom : 24px;
+`
 
 const TravelsPage = () => {
 
@@ -14,7 +71,42 @@ const TravelsPage = () => {
     return (
         <>
             <Header/>
-            <div>여행</div>
+            <TravelDiv>
+                <OngoingTravelDiv>
+                    <CategoryP>
+                        진행중인 여행
+                    </CategoryP>
+                    <TravelCard></TravelCard>
+                </OngoingTravelDiv>
+                <UpcomingTravelDiv>
+                    <CategoryP>
+                        다가오는 여행
+                    </CategoryP>
+                    <TravelCardDiv>
+                        <UpcomingTravelCard></UpcomingTravelCard>
+                    </TravelCardDiv>
+                </UpcomingTravelDiv>
+                <CompleteTravelDiv>
+                    <CategoryP>
+                        지난 여행
+                    </CategoryP>
+                    <TravelCardDiv>
+                        <CompleteTravelCard></CompleteTravelCard>
+                        <CompleteTravelCard></CompleteTravelCard>
+                        <CompleteTravelCard></CompleteTravelCard>
+                        <CompleteTravelCard></CompleteTravelCard>
+                        <CompleteTravelCard></CompleteTravelCard>
+                        <CompleteTravelCard></CompleteTravelCard>
+                        <CompleteTravelCard></CompleteTravelCard>
+                        <CompleteTravelCard></CompleteTravelCard>
+                    </TravelCardDiv>
+                </CompleteTravelDiv>
+                <CreateTravelDiv>
+                    <Link to="/travels/create">
+                        <CreateBtn></CreateBtn>                    
+                    </Link>
+                </CreateTravelDiv>
+            </TravelDiv>
         </>
     );
 };
