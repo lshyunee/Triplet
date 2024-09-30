5f720cc31c454c0d1fc03d8ad2b4034d9d5555eb
hyun <a94460093@gmail.com>
2024-09-30 11:07:33 +0900

    refactor: 이미지 파일 크기 제한 추가 - 파일 크기 제한 에러 처리 추가 - 여행 수정 시 이미지도 수정 할 경우 s3에 기존 이미지 삭제 로직 추가 Closes #55

--- triplet/src/main/java/com/ssafy/triplet/exception/controller/CustomExceptionHandler.java
+++ triplet/src/main/java/com/ssafy/triplet/exception/controller/CustomExceptionHandler.java
@@ -1,11 +1,13 @@
 package com.ssafy.triplet.exception.controller;
 
+import com.ssafy.triplet.exception.CustomErrorCode;
 import com.ssafy.triplet.exception.CustomException;
 import com.ssafy.triplet.response.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 @Slf4j
 @RestControllerAdvice
@@ -19,6 +21,15 @@ public class CustomExceptionHandler {
         return ResponseEntity
                 .status(e.getStatus())
                 .body(new ApiResponse<Void>(errorCode, message));
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<?> handleMaxSizeException(MaxUploadSizeExceededException e) {
+        log.info("[handleMaxSizeException] = 파일 크기 초과");
+        CustomErrorCode errorCode = CustomErrorCode.MAX_UPLOAD_SIZE_EXCEEDED;
+        return ResponseEntity
+                .status(errorCode.getStatus())
+                .body(new ApiResponse<Void>(errorCode.getCode(), errorCode.getMessage()));
     }
 
 }
