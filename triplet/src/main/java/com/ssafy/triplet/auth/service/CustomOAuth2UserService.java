39eac44fd57e635dd1e0d9f2db98fa86f518599a
enghddd <jung1213546@gmail.com>
2024-10-02 17:37:05 +0900

    !HOTFIX: 로그 추가

--- triplet/src/main/java/com/ssafy/triplet/auth/service/CustomOAuth2UserService.java
+++ triplet/src/main/java/com/ssafy/triplet/auth/service/CustomOAuth2UserService.java
@@ -4,12 +4,14 @@ import com.ssafy.triplet.auth.dto.*;
 import com.ssafy.triplet.member.entity.Member;
 import com.ssafy.triplet.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
@@ -32,12 +34,14 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         String username = oAuth2Response.getProvider() + " " + oAuth2Response.getProviderId();
 
         Member existData = memberRepository.findByMemberId(username);
+        log.info("existData = {}", existData);
 
         if (existData == null) {
             Member member = Member.builder()
                     .memberId(username)
                     .role("ROLE_USER")
                     .build();
+            log.info("existData22222 = {}", existData);
 
             memberRepository.save(member);
             MemberAuthDto memberAuthDto = new MemberAuthDto(username, "ROLE_USER");
