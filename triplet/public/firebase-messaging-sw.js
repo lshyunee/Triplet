a229269921a392f6fcb425c697f08197be3f0855
jinseobYun <wlstjq447@naver.com>
2024-10-06 15:40:21 +0900

    Refactor: push알림 수정 중

--- triplet/public/firebase-messaging-sw.js
+++ triplet/public/firebase-messaging-sw.js
@@ -16,48 +16,6 @@ firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
 
-const CACHE_NAME = 'my-app-cache';
-const urlsToCache = ['/', '/index.html', '/static/js/main.js'];
-
-// Install Event
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-// Fetch Event
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
-});
-
-// Activate Event
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
 self.addEventListener("push", function (e) {
   console.log(e.data.text())
 
@@ -69,12 +27,6 @@ self.addEventListener("push", function (e) {
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
 
-// Handle messages to skip waiting
-self.addEventListener('message', (event) => {
-  if (event.data === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
 self.addEventListener("notificationclick", function (event) {
   const url = "/";
   event.notification.close();
