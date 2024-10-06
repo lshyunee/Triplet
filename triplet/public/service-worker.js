ad8e84cc1758b50a5767be6eb51a08e03c120196
jinseobYun <wlstjq447@naver.com>
2024-10-06 17:14:54 +0900

    Refactor: service-worker 수정

--- triplet/public/service-worker.js
+++ triplet/public/service-worker.js
@@ -43,9 +43,3 @@ self.addEventListener('activate', (event) => {
     })
   );
 });
-// 워커 파일이 업데이트되면 사용자에게 알림
-self.addEventListener('message', (event) => {
-  if (event.data === 'SKIP_WAITING') {
-      self.skipWaiting();
-  }
-});
