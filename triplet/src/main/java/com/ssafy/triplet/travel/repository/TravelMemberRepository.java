76cf75e7f6d76e36be728c320f6c6dbb73ec45c9
jinseobYun <wlstjq447@naver.com>
2024-10-08 00:23:15 +0900

    Refactor: push 알림 예산 사용 50% 80% 초과 Push 알림
    Fix: #feat/61_fcm

--- triplet/src/main/java/com/ssafy/triplet/travel/repository/TravelMemberRepository.java
+++ triplet/src/main/java/com/ssafy/triplet/travel/repository/TravelMemberRepository.java
@@ -1,11 +1,15 @@
 package com.ssafy.triplet.travel.repository;
 
+import com.ssafy.triplet.member.entity.Member;
 import com.ssafy.triplet.travel.entity.TravelMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -17,4 +21,12 @@ public interface TravelMemberRepository extends JpaRepository<TravelMember, Long
     void deleteByMemberIdAndTravelId(Long memberId, Long travelId);
 
     int countByTravelId(Long travelId);
+    @Query("SELECT m FROM Member m " +
+            "WHERE m.id IN (" +
+            "   SELECT tm.member.id FROM TravelMember tm " +
+            "   WHERE tm.travel.id = :travelId" +
+            ") " +
+            "AND m.isNotificationEnabled = true")
+    List<Member> findMembersByTravelIdAndNotificationEnabled(@Param("travelId") Long travelId);
+
 }
