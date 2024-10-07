e3550639537601fbdb8e3ca4bcc4860e32a14281
jinseobYun <wlstjq447@naver.com>
2024-10-07 14:15:52 +0900

    Fix: 서버시간확인용

--- triplet/src/App.tsx
+++ triplet/src/App.tsx
@@ -50,6 +50,7 @@ const App: React.FC = () => {
       "/mypage/info-set"
     ];
 
+    console.log(new Date())
     const loginSuccessPages = ["/login", "/signup"];
 
     if (offPages.includes(location.pathname)) {
