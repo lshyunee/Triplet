0efc14ceb257ca4ddd0e98f6db729a696ffeb876
jinseobYun <wlstjq447@naver.com>
2024-10-07 15:39:27 +0900

    Feat: 공유중인 여행 상세페이지
    수정중

--- triplet/src/store.ts
+++ triplet/src/store.ts
@@ -5,6 +5,7 @@ import ongoingTravelSlice from './features/travel/ongoingTravelSlice';
 import upcomingTravelSlice from './features/travel/upcomingTravelSlice';
 import completedTravelSlice from './features/travel/completedTravelSlice';
 import snsTravelSlice from './features/travel/snsTravelSlice';
+import sharedTravelSlice from './features/travel/shareTravelSlice';
 import userInfoSlice from './features/user/userInfoSlice';
 
 // 스토어 생성
@@ -17,6 +18,7 @@ const store = configureStore({
     completedTravel : completedTravelSlice,
     snsTravel : snsTravelSlice,
     userInfo : userInfoSlice,
+    sharedTravel : sharedTravelSlice,
   },
 });
 
